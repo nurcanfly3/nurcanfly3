9fad36b1aee736ad61b8bbddaf874324112179f2
nurcanfly3 <62663681+nurcanfly3@users.noreply.github.com>
2020-03-25 21:19:19 +0300

    Add files via upload

---
+++ CollectionTracking.DataAccess/Concrete/EntityFramework/CollectionTrackingContext.cs
@@ -0,0 +1,14 @@
+﻿using CollectionTracking.Entities.Concrete;
+using System.Data.Entity;
+
+namespace CollectionTracking.DataAccess.Concrete.EntityFramework
+{
+    public class CollectionTrackingContext : DbContext
+    {
+        public CollectionTrackingContext() : base("CollectionTrackingDb")
+        {
+            Database.SetInitializer<CollectionTrackingContext>(null);
+        }
+        public DbSet<Film> Films { get; set; }
+    }
+}
